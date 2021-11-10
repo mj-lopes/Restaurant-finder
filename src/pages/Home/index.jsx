97ef8409d746bf7e86fc51f24c9ad5bec8154fdd
mlrJunior <marcos_lopes_ribeiro_junior@hotmail.com>
2021-11-09 23:55:20 -0300

    Alterado o método de importação para named export

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -16,21 +16,21 @@ import {
 import logo from "../../assets/logo.svg";
 import restaurante from "../../assets/restaurante-fake.png";
 
-import {
-  ImageCard,
-  Loader,
-  Map,
-  RestaurantCard,
-  Skeleton,
-  Modal,
-} from "../../components";
+// import {
+//   ImageCard,
+//   Loader,
+//   Map,
+//   RestaurantCard,
+//   Skeleton,
+//   Modal,
+// } from "../../components";
 
-// import { ImageCard } from "../../components/ImageCard";
-// import { Loader } from "../../components/Loader";
-// import Map from "../../components/Map";
-// import { RestaurantCard } from "../../components/RestaurantCard";
-// import { Skeleton } from "../../components/Skeleton";
-// import { Modal } from "../../components/Modal";
+import ImageCard from "../../components/ImageCard";
+import Loader from "../../components/Loader";
+import Map from "../../components/Map";
+import RestaurantCard from "../../components/RestaurantCard";
+import Skeleton from "../../components/Skeleton";
+import Modal from "../../components/Modal";
 
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
