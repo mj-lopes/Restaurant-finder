f9027ed02feb4c999637a1f22c1e31e5f6a51714
mlrJunior <marcos_lopes_ribeiro_junior@hotmail.com>
2021-11-10 00:09:26 -0300

    Removidos todos arquivos de modulos

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,3 @@
-// export * from "./ImageCard";
-// export * from "./Loader";
-// export { default as Map } from "./Map";
-// export * from "./Modal";
-// export * from "./RestaurantCard";
-// export * from "./Skeleton";
-
 export { default as ImageCard } from "./ImageCard";
 export { default as RestaurantCard } from "./RestaurantCard";
 export { default as Modal } from "./Modal";
